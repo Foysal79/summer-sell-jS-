7197537f5c9fa57aebc0983ec42b1e6860eab8dd
Foysal79 <onthor312001@gmail.com>
2023-08-21 06:06:12 +0600

    problem remove

--- js/index.js
+++ js/index.js
@@ -142,36 +142,3 @@ function flexibleSofa()
 {
    mainFunction('flexibleSofaName', 'flexibleSofaPrice');
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // add to calculation entry
-// function addToCalculationEntry(areaType, area)
-// {
-//      console.log(areaType + " " + area);
-//      const calculationEntry = document.getElementById('calculation-entry');
-//      const count = calculationEntry.childElementCount;
-     
-//      const p = document.createElement('p');
-//      p.classList.add('my-4');
-//      p.innerHTML = `${count  + 1} ${areaType} ${area} cm<sup>2</sup> <button class="btn btn-sm btn-success" >Convert</button> `;
-//      calculationEntry.appendChild(p);
-
-
-
-// }
